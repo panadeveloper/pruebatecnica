cbc9b29c3f99c08e49fad6de67e9450630aa95c3
panadeveloper <165926903+panadeveloper@users.noreply.github.com>
2024-11-22 09:42:54 -0500

    Delete Backend directory
    Delete Backend directory

--- Backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import DatabaseConfig
-
-
-class DatabaseConfigSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DatabaseConfig
-        fields = ['host', 'port', 'namedb', 'user', 'password', 'query']
--- Backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import DataExtractionView
-
-urlpatterns = [
-    path('extract-data/', DataExtractionView.as_view(), name='extract-data'),
-]
